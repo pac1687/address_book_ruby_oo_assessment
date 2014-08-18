7200c20f109b57589aae5d101b831edfd979b0bc
Justin Pacubas <jpacubas@gmail.com>
2014-08-17 18:37:27 -0700

    Pass first spec for initializing a new instance of Contact.

---
+++ lib/contact.rb
@@ -0,0 +1,6 @@
+class Contact
+
+	def initialize(name)
+		@name = name
+	end
+end
---
+++ spec/contact_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require 'contact'
+
+describe Contact do
+	it 'initializes with a name' do
+		test_name = Contact.new('John Doe')
+		expect(test_name).to be_an_instance_of Contact
+	end
+end
