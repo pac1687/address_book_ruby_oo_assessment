fdb17956585a3160b276be9fe3d0dff99e102bff
Justin Pacubas <jpacubas@gmail.com>
2014-08-17 19:06:46 -0700

    Add ability to add and output a phone number.

--- lib/contact.rb
+++ lib/contact.rb
@@ -2,9 +2,18 @@ class Contact
 
 	def initialize(name)
 		@name = name
+		@phone = []
 	end
 
 	def name
 		@name
+	end
+
+	def phone
+		@phone
+	end
+
+	def add_phone(phone)
+		@phone << phone
 	end
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -1,14 +1,22 @@
 require 'rspec'
 require 'contact'
+require 'pry'
 
 describe Contact do
 	it 'initializes with a name' do
-		test_name = Contact.new('John Doe')
-		expect(test_name).to be_an_instance_of Contact
+		test_contact = Contact.new('John Doe')
+		expect(test_contact).to be_an_instance_of Contact
 	end
 
 	it 'outputs the name' do
-		test_name = Contact.new('John Doe')
-		expect(test_name.name).to eq 'John Doe'
+		test_contact = Contact.new('John Doe')
+		expect(test_contact.name).to eq 'John Doe'
+	end
+
+	it 'adds a phone number' do
+		test_contact = Contact.new('John Doe')
+		test_phone = '555-123-4567'
+		test_contact.add_phone(test_phone)
+		expect(test_contact.phone).to eq ['555-123-4567']
 	end
 end
