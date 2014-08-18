d5605c3dcfcf6c96c8626dda5c86181cf4811f50
Justin Pacubas <jpacubas@gmail.com>
2014-08-17 18:42:16 -0700

    Add functionality to output name.

--- lib/contact.rb
+++ lib/contact.rb
@@ -3,4 +3,8 @@ class Contact
 	def initialize(name)
 		@name = name
 	end
+
+	def name
+		@name
+	end
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -6,4 +6,9 @@ describe Contact do
 		test_name = Contact.new('John Doe')
 		expect(test_name).to be_an_instance_of Contact
 	end
+
+	it 'outputs the name' do
+		test_name = Contact.new('John Doe')
+		expect(test_name.name).to eq 'John Doe'
+	end
 end
