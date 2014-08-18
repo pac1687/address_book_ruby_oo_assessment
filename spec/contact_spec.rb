fa024e84d5f0c9573f441b17a49521e83362842d
Justin Pacubas <jpacubas@gmail.com>
2014-08-17 19:39:24 -0700

    Begin implementing address class instance in Contact class method.

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -23,8 +23,8 @@ describe Contact do
 
 	it 'adds an address' do
 		test_contact = Contact.new('John Doe')
-		test_address = '123 Main Street'
-		test_contact.add_address(test_address)
+		test_address = Address.new('123 Main Street')
+		test_contact.add_address(test_address.address)
 		expect(test_contact.address).to eq ['123 Main Street']
 	end
 
@@ -38,10 +38,10 @@ describe Contact do
 	it 'saves the contact' do
 		test_contact = Contact.new('John Doe')
 		test_phone = Phone.new('555-123-4567')
-		test_address = '123 Main Street'
+		test_address = Address.new('123 Main Street')
 		test_email = 'jdoe@gmail.com'
 		test_contact.add_phone(test_phone.phone)
-		test_contact.add_address(test_address)
+		test_contact.add_address(test_address.address)
 		test_contact.add_email(test_email)
 		test_contact.save
 		expect(Contact.all).to eq [test_contact]
